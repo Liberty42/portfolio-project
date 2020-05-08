e4ef62ec65a6ecf8341e76efc20cc10f38c4ea5a
Robert Nichols <bob@iMac.local>
2020-05-08 16:33:00 -0400

    thanks Nick

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD':'WeAreVan!t!3s',
+        'PASSWORD':'django1234',
         'HOST':'localhost',
         'PORT':'5432',
     }
